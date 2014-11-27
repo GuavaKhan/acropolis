6c89c309d6c5f1fb521b8b772b20ba6125a30f57
Jonathan Vronsky <jonathan.vronsky@gmail.com>
2014-11-27 12:54:19 -0800

    ROC grapher working and complete

---
+++ analysis/ROC/ROC.m
@@ -0,0 +1,12 @@
+function ROC(output, target)
+  clf;
+  % Source analysisTools directory
+  source("../analysisTools/confusionMatrix.m");
+		ROCPoints = ROCLoopValues(output, target);
+    for i = 1:size(ROCPoints)(1)
+     subplot(size(target)(2), 1, i);
+     hold on;
+     plot((1-ROCPoints{i}(:,2)), ROCPoints{i}(:,1), 'r*');
+     hold off;
+    end
+end
---
+++ analysis/ROC/ROCLoopValues.m
@@ -0,0 +1,57 @@
+%This function generates the ROC data points to graph. 
+%Output-the actual output from the predictor (this is raw value
+%	meaning not 0's and 1's but the actual percentages given by the predictor.
+%Target- the actual class the corespond to the output, this vector is in 0's and 1'
+function [ROCPoints] = ROCLoopValues(output, target)
+  minimum = min(output);
+  maximum = max(output);
+	thresholds = zeros(1, size(target)(2));
+	% Find the minimum differece in the threshold.
+	% For each column
+	for i = 1:size(target)(2)
+		minThershold = inf;
+		% Find the smallest difference.
+		for j = 1:size(target)(1)
+			for k = 1:size(target)(1)
+				% Find the difference.
+				difference = abs(output(j, i) - output(k, i));
+				if(difference < minThershold)
+					if(difference > 1e-4)
+						minThershold = difference;
+					end
+				end
+			end
+		end
+		thresholds(1, i) = minThershold;
+	end
+	ROCPoints = cell(size(target)(2), 1);
+	for i = 1:size(target)(2)
+		% Init threshold.
+		threshold = minimum(1, i);
+		% NumberOfValues to compute for ROC graph.
+		numberOfValues = ceil(((maximum(1, i) - minimum(1, i)) / thresholds(1, i)));
+		values = zeros(numberOfValues, 2);
+		%Initiate for loop
+		thresholdOutput = output(:,i) > threshold;
+		valueCount = 0;
+		% Go till maximum value or a bit above.
+		thresholds
+		maximum(1,1)
+		minimum(1,1)
+		while (threshold <= (maximum(1, i) + thresholds(1, i)))
+			valueCount += 1;
+			%Compute confusion matrix
+			confusionM = confusionMatrix(target(:, i), thresholdOutput);
+			% Compute points for that threshold
+			sensativity = confusionM(1, 1) / (confusionM(1, 1) + confusionM(1, 2));
+      specificity = confusionM(2, 2) / (confusionM(2, 1) + confusionM(2, 2));
+			values(valueCount,:) = [sensativity, specificity];
+			%Increment threshold by minimum amount
+			threshold = threshold + thresholds(1, i);
+			thresholdOutput = output(:,i) > threshold;
+		end
+		% add ROCPoints set
+		ROCPoints{i} = values;
+	end
+
+end
