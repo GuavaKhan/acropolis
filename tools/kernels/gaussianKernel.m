3865e6f3fa6a44d6ad3f9f1f95869069c1b70515
Shawn Shojaie <shojaie.mac@gmail.com>
2014-11-30 17:35:10 -0800

    Add and finish GaussianKernel function.

--- tools/kernels/gaussianKernel.m
+++ tools/kernels/gaussianKernel.m
@@ -1,4 +1,14 @@
+% Returns value of the gaussian kernel computation run on two samples
+% sample1 and sample2 are two vectors from the input space e.g. a single sample
+% with any subset of the feature space
+% varargin contains the optional inputs pass inputs in as name/value pairs
+% e.g. ('option_name', option_value)
+% sigmas values can vary the performance of the hyperplane from almost
+% linear behavior, no different from using LinearKernel(), to overfitting
+% the data like PolynomialKernel() run with extremely high orders
 
+% sample usage: 
+% GaussianKernel([2 4], [6 5], 'sigma', 7) = 0.84074
 
 
 function kernel_matrix_element = GaussianKernel(sample1, sample2, varargin)
@@ -20,7 +30,7 @@ function kernel_matrix_element = GaussianKernel(sample1, sample2, varargin)
 
 	assert(isrow(sample2), ['Second input to LinearKernel() was not a ' ...
 		'single sample. Its dimensions were: %dx%d'], size(sample2) );
-	
+
 	% if false the matrix operations will fail
 	assert(size(sample1) == size(sample2), 'Samples were not same size.');
 
@@ -32,8 +42,13 @@ function kernel_matrix_element = GaussianKernel(sample1, sample2, varargin)
 		end
 	end
 
+	% set optional valriables
+	sigma = opts.('sigma');
 
-
+	% the actual kernel function
+	temp = (norm(sample1 - sample2)) .^ 2;
+	temp = temp / ( 2 * (sigma .^ 2) );
+	kernel_matrix_element = exp( - temp);
 
 end
 
